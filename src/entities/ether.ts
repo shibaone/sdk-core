59932f976c14fd45df66ac6bf34d4a7bdabe571e
Oleh <Oleh.Siloch@shib.io>
2025-08-20 11:40:12 +0300

    fix: add chains

--- src/entities/ether.ts
+++ src/entities/ether.ts
@@ -10,10 +10,44 @@ import { ChainId } from '../chains'
  */
 export class Ether extends NativeCurrency {
   protected constructor(chainId: number) {
-    if (chainId === ChainId.SHIBARIUM || chainId === ChainId.PUPPYNET)
-      super(chainId, 18, 'BONE', 'BONE SHIBASWAP')
-    else
-      super(chainId, 18, 'ETH', 'Ether')
+    // Handle different chains with their specific native currencies
+    switch (chainId) {
+      case ChainId.SHIBARIUM:
+      case ChainId.PUPPYNET:
+        super(chainId, 18, 'BONE', 'BONE SHIBASWAP')
+        break
+      case ChainId.BNB:
+      case ChainId.BNB_TESTNET:
+        super(chainId, 18, 'BNB', 'Binance Coin')
+        break
+      case ChainId.AVALANCHE:
+      case ChainId.AVALANCHE_FUJI:
+        super(chainId, 18, 'AVAX', 'Avalanche')
+        break
+      case ChainId.POLYGON:
+      case ChainId.POLYGON_MUMBAI:
+      case ChainId.POLYGON_AMOY:
+        super(chainId, 18, 'MATIC', 'Matic')
+        break
+      case ChainId.GNOSIS:
+      case ChainId.GNOSIS_CHIADO:
+        super(chainId, 18, 'XDAI', 'xDai')
+        break
+      case ChainId.MOONBEAM:
+        super(chainId, 18, 'GLMR', 'Glimmer')
+        break
+      case ChainId.CELO:
+      case ChainId.CELO_ALFAJORES:
+        super(chainId, 18, 'CELO', 'Celo')
+        break
+      case ChainId.MANTLE_TESTNET:
+        super(chainId, 18, 'MNT', 'Mantle')
+        break
+      default:
+        // Default to ETH for all other chains (Ethereum mainnet, testnets, L2s, etc.)
+        super(chainId, 18, 'ETH', 'Ether')
+        break
+    }
   }
 
   public get wrapped(): Token {
